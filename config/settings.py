8fc04edfb1b7e9b5790fc162d1559d6883a8d9e7
Frank <3438835284@qq.com>
2026-02-24 18:12:22 +0800

    提高安全性

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-54g%bp87+ng=aqx*))3k_5fy+e7i+zs^@_6yf_huf8m0r7*enp'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
